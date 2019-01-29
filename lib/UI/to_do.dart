fe608478a3e5d5b5ac2b4772194bb9ce1bac168d
sctajc <sctajc@gmail.com>
2019-01-29 16:57:59 +1300

    Menu working and to_do established

--- lib/UI/to_do.dart
+++ lib/UI/to_do.dart
@@ -5,7 +5,7 @@ class ToDo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('To do list. Things withoutll times'),
+        title: Text('To do list. Things without times'),
       ),
       body: Center(
         child: RaisedButton(
